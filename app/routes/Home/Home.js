5f116e55234e574ec43dd31e50eab843ee039888
Karolis Malcevicius <karolis.tinex@gmail.com>
2017-08-09 08:47:14 +0300

    Initial commit

---
+++ app/routes/Home/Home.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { View } from 'react-native';
+
+// import HomeContainer from './HomeContainer';
+
+export default () => <View />;
---
+++ app/routes/Home/styles.js
@@ -0,0 +1,12 @@
+import EStyleSheet from 'react-native-extended-stylesheet';
+
+const styles = EStyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default styles;
