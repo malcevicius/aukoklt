a42253b054fdd333312ad10211f5c9425ba5cf1d
Karolis Malcevicius <karolis.tinex@gmail.com>
2017-10-27 19:38:23 +0300

    Fixed the bug where android wix native navigation wasn't installed correctly with facebook fsdk

--- android/app/src/main/java/com/aukoklt/MainActivity.java
+++ android/app/src/main/java/com/aukoklt/MainActivity.java
@@ -4,16 +4,6 @@ import com.reactnativenavigation.controllers.SplashActivity;
 import android.content.Intent;
 
 public class MainActivity extends SplashActivity {
-
-    /**
-     * Returns the name of the main component registered from JavaScript.
-     * This is used to schedule rendering of the component.
-     */
-    @Override
-    protected String getMainComponentName() {
-        return "aukoklt";
-    }
-
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
